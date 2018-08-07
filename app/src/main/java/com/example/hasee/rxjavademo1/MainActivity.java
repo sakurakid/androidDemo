1be3b406c842cb00cb75a3759e7dcbf06d0fd960
sakurakid <610444099@qq.com>
2018-08-07 22:08:24 +0800

    学了Rxjava的操作符

--- app/src/main/java/com/example/hasee/rxjavademo1/MainActivity.java
+++ app/src/main/java/com/example/hasee/rxjavademo1/MainActivity.java
@@ -7,6 +7,10 @@ import android.util.Log;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.BiFunction;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -118,33 +122,103 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
+//        Observable<Integer> observable1 = Observable.create(new ObservableOnSubscribe<Integer>() {
+//            @Override
+//            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+//                Log.d("233","emit 1");
+//                e.onNext(1);
+//                Thread.sleep(1000);
+//
+//                Log.d("233","emit 2");
+//                e.onNext(2);
+//                Thread.sleep(1000);
+//                Log.d("233","emit 3");
+//                e.onNext(3);
+//                Thread.sleep(1000);
+//                Log.d("233","emit 4");
+//                e.onNext(4);
+//                Thread.sleep(1000);
+//                Log.d("233","emit complete1");
+//                e.onComplete();
+//            }
+//        }).subscribeOn(Schedulers.io());
+//
+//        Observable<String> observable2 = Observable.create(new ObservableOnSubscribe<String>() {
+//            @Override
+//            public void subscribe(ObservableEmitter<String> e) throws Exception {
+//                Log.d("233","emit A");
+//                e.onNext("A");
+//                Thread.sleep(1000);
+//                Log.d("233","emit B");
+//                e.onNext("B");
+//                Thread.sleep(1000);
+//                Log.d("233","emit C");
+//                e.onNext("C");
+//                Thread.sleep(1000);
+//                Log.d("233","emit complete2");
+//                e.onComplete();
+//            }
+//        }).subscribeOn(Schedulers.io());
+//        Observable.zip(observable1, observable2, new BiFunction<Integer, String, String>() {
+//            @Override
+//            public String apply(Integer integer, String s) throws Exception {
+//                return integer + s;
+//            }
+//        }).subscribe(new Observer<String>() {
+//            @Override
+//            public void onSubscribe(Disposable d) {
+//                Log.d("233","onSubscribe");
+//            }
+//
+//            @Override
+//            public void onNext(String value) {
+//                Log.d("233","onNext"+value);
+//
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//                Log.d("233","OnErron");
+//
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//                Log.d("233","oncomplete");
+//
+//            }
+//        });
         Observable<Integer> observable1 = Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
             public void subscribe(ObservableEmitter<Integer> e) throws Exception {
-                Log.d("233","emit 1");
-                e.onNext(1);
-                Log.d("233","emit 2");
-                e.onNext(2);
-                Log.d("233","emit 3");
-                e.onNext(3);
-                Log.d("233","emit 4");
-                e.onNext(4);
-                Log.d("233","emit complete1");
-                e.onComplete();
+                for(int i = 0;;i++){
+                    e.onNext(i);
+                }
             }
-        });
+        }).subscribeOn(Schedulers.io());
 
         Observable<String> observable2 = Observable.create(new ObservableOnSubscribe<String>() {
             @Override
             public void subscribe(ObservableEmitter<String> e) throws Exception {
-                Log.d("233","emit A");
                 e.onNext("A");
-                Log.d("233","emit B");
-                e.onNext("B");
-                Log.d("233","emit C");
-                e.onNext("C");
-                Log.d("233","emit complete2");
-                e.onComplete();
+            }
+        }).subscribeOn(Schedulers.io());
+
+        Observable.zip(observable1, observable2, new BiFunction<Integer, String, String>() {
+            @Override
+            public String apply(Integer integer, String s) throws Exception {
+                return integer+s;
+            }
+        }).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                Log.d("233", s);
+            }
+
+        }, new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Exception {
+                Log.w("233",throwable);
             }
         });
   }
